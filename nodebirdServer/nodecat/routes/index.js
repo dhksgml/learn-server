83e752a996747047cb61978d61449bf96dee229c
dhksgml <69703890+dhksgml@users.noreply.github.com>
2021-10-12 21:11:40 +0900

    웹 API 서버 실습

--- nodebirdServer/nodecat/routes/index.js
+++ nodebirdServer/nodecat/routes/index.js
@@ -57,7 +57,7 @@ router.get('/test', async (req,res,next)=>{
     try{
         if(!req.session.jwt){
             //세션에 토큰이 없으면 토큰 발급 시도
-            const tokenResult = await axios.post('http://localhost:8002/v1/token',{
+            const tokenResult = await axios.post('http://localhost:8002/v2/token',{
                 clientSecret: process.env.CLIENT_SECRET,
             });
             if(tokenResult.data && tokenResult.data.code === 200){
@@ -69,7 +69,7 @@ router.get('/test', async (req,res,next)=>{
             }
         }
         //발급받은 토큰 테스트
-        const result = await axios.get('http://localhost:8002/v1/test',{
+        const result = await axios.get('http://localhost:8002/v2/test',{
             headers:{ authorization: req.session.jwt },
         });
         return res.json(result.data);
@@ -81,6 +81,10 @@ router.get('/test', async (req,res,next)=>{
         }
         return next(error);
     }
+});
+
+router.get('/',(req,res)=>{
+    res.render('main',{ key:process.env.CLIENT_SECRET });
 });
 
 module.exports = router;
