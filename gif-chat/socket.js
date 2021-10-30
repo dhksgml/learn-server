fbb3cfb9a0bacaeceac2d57422425745285b5f03
dhksgml <69703890+dhksgml@users.noreply.github.com>
2021-10-30 18:06:39 +0900

    웹 소켓으로 실시간 데이터 전송하기(Socket.IO 사용)

--- gif-chat/socket.js
+++ gif-chat/socket.js
@@ -1,31 +1,29 @@
-const WebSocket = require('ws');
-// const SocketIO = require('socket.io');
+const SocketIO = require('socket.io');
 
 module.exports = (server) => {
-    const wss = new WebSocket.Server({ server });
+    const io = SocketIO(server, { path: '/socket.io' });
 
-    wss.on('connection',(ws,req)=>{
+    io.on('connection', (socket) =>{
         //웹 소켓 연결 시
+        const req = socket.request;
         const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-        console.log('새로운 클라이언트 접속',ip);
-        ws.on('message',(message)=>{
-            //클라이언트로부터 메시지 수신 시
-            console.log(""+message); // <Buffer ed ... 4 more bytes>가 뜰 경우 message 앞에 ""를 붙여준다
+        console.log('새로운 클라이언트 접속!', ip, socket.id, req.ip);
+        socket.on('disconnect', () => {
+            //연결 종료 시
+            console.log('클라이언트 접속 해제', ip, socket.id);
+            clearInterval(socket.interval);
         });
-        ws.on('error',(error)=>{
+        socket.on('error',(error) => {
             //에러 시
             console.error(error);
-        })
-        ws.on('close',()=>{
-            //연결 종료 시
-            console.log('클라이언트 접속 해제',ip);
-            clearInterval(ws.interval);
         });
-
-        ws.interval = setInterval(()=>{
-            if(ws.readyState === ws.OPEN){
-                ws.send('서버에서 클라이언트로 메시지를 보냅니다.');
-            }
-        },3000);
+        socket.on('reply', (data) => {
+            //클라이언트로부터 메시지 수신 시
+            console.log(data);
+        });
+        socket.interval = setInterval(() => {
+            // 3초마다 클라이언트로 메시지 전송
+            socket.emit('news', 'Hello Socket.IO');
+        }, 3000);
     });
 };
