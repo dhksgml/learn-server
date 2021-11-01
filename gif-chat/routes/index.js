39a7891961170422b22a1a696f73325ffe2d8a77
dhksgml <69703890+dhksgml@users.noreply.github.com>
2021-11-01 17:34:09 +0900

    gif-chat 실습 완료 (gif 파일 저장)
    multer 패키지를 사용하여 gif를 uploads라는 폴더에 저장

--- gif-chat/routes/index.js
+++ gif-chat/routes/index.js
@@ -1,4 +1,8 @@
 const express = require('express');
+const multer = require('multer');
+const path = require('path');
+const fs = require('fs');
+
 
 const Room = require('../schemas/room');
 const Chat = require('../schemas/chat');
@@ -88,6 +92,39 @@ router.post('/room/:id/chat', async(req, res, next) => {
         req.app.get('io').of('/chat').to(req.params.id).emit('chat',chat);
         res.send('ok');
     } catch (error) {
+        console.error(error);
+        next(error);
+    }
+});
+
+try{
+    fs.readdirSync('uploads');
+}catch(err){
+    console.error('uploads 폴더가 없어 uploads 폴더를 생성합니다.');
+    fs.mkdirSync('uploads');
+}
+const upload = multer({
+    storage: multer.diskStorage({
+        destination(req, file, done){
+            done( null, 'uploads/');
+        },
+        filename(req, file, done){
+            const ext = path.extname(file.originalname);
+            done(null, path.basename(file.originalname,ext)+Date.now() + ext);
+        },
+    }),
+    limits: { fileSize: 5 * 1024 * 1024 },
+});
+router.post('/room/:id/gif', upload.single('gif'), async(req,res,next) => {
+    try{
+        const chat = await Chat.create({
+            room: req.params.id,
+            user: req.session.color,
+            gif: req.file.filename,
+        });
+        req.app.get('io').of('/chat').to(req.params.id).emit('chat',chat);
+        res.send('ok');
+    }catch(error){
         console.error(error);
         next(error);
     }
