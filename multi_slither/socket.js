0a5d2bd784211bfd597a02c7ceeab29db7e30462
dhksgml <69703890+dhksgml@users.noreply.github.com>
2021-11-09 01:20:26 +0900

    멀티 지렁이 (구현 중)

--- multi_slither/socket.js
+++ multi_slither/socket.js
@@ -1,24 +1,36 @@
 const SocketIO = require('socket.io');
 
+const isSnakeArr = [];
+let isConnected = false;
+
 module.exports = (server) => {
     const io = SocketIO(server, {path: '/socket.io'});
 
     io.on('connection', (socket) => {
+       
+        if(isConnected==true){
+            return;
+        }
         const req = socket.request;
         const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
         console.log('새로운 클라이언트 접속!', ip, socket.id, req.ip);
+        isConnected = true;
+
         socket.on('disconnect', () =>{
             console.log('클라이언트 접속 해제', ip, socket.id);
             clearInterval(socket.interval);
+            isConnected = false;
         });
         socket.on('error',(error)=>{
             console.error(error);
         })
         socket.on('reply',(data)=>{
-            console.log(data);
+            // console.log(data);
+            isSnakeArr[0] = data;
+            console.log(isSnakeArr)
         })
         socket.interval = setInterval(()=>{
-            socket.emit('news',socket.id);
-        }, 3000);
+            socket.emit('news',isSnakeArr[0]);
+        }, 500);
     });
 };
