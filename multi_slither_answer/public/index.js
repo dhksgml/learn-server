579051aefe9d50bd9cd0620d9580183a6644378c
dhksgml <69703890+dhksgml@users.noreply.github.com>
2021-12-04 16:51:10 +0900

    멀티 지렁이 프로토타입(멀티 구현 완료)
    먹이와 지렁이에 대한 정보가 담긴 배열을 주고 받게 구현이 되었습니다.
    자신의 아이디와 같은지 판단해서 같을 경우에만 조작 가능하도록 변경하니 구현하고자 했던 방향으로 구현이 되었습니다.

--- multi_slither_answer/public/index.js
+++ multi_slither_answer/public/index.js
@@ -2,38 +2,40 @@
 
 var vcanvas, ctx;
 // var r_left, r_right, r_up, r_down;
-var key = {left:0,right:0,up:0,down:0}
-var feedArr = []; // 먹이 배열
-var SnakeArr = []; //뱀 배열
-var Snake; //각각의 뱀
-var scl = 10;
-var socket = io();
+var key = {left:0,right:0,up:0,down:0}  //조작키
+var feedArr = [];                       //먹이 배열
+var SnakeArr = [];                      //뱀 배열
+var Snake;                              //클라이언트 자신의 뱀 객체
+var scl = 10;                           //뱀의 크기
+var socket = io();                      //html에서 경로 지정한 웹 소켓 생성
 
-var SnakeMove = {x:null, y:null, xv:null, yv:null}
-
+//클라이언트 뱀에 대한 정보를 서버로 보내는 함수
 function sendData() {
-    socket.emit('snakeLocation', Snake);
+    socket.emit('snakeLocation', Snake); //서버에 있는 snakeLocation 이벤트 실행
 }
 
+//클라이언트 뱀 객체
 var Snake = function() {
-    this.id;
-    this.x = 0;
-    this.y = 0;
-    this.xv = 0;
-    this.yv = 0;
-    this.tailArr = [];
-    this.feedArr = [];
+    this.id;            //아이디
+    this.x = 0;         //뱀의 x좌표
+    this.y = 0;         //뱀의 y좌표
+    this.xv = 0;        //뱀의 x축 방향
+    this.yv = 0;        //뱀의 y축 방향
+    this.tailArr = [];  //뱀 꼬리
 
+    //서버에서 id를 받아오기 위한 이벤트 - (this.id 선언할 때 soket.io로 초기화 하면 실행 순서 때문에 undefined 값이 들어감)
     socket.on('getId',(id) => {
         this.id = id
     })
 
+    //꼬리를 증가시키는 함수
     this.addTail = function() {
         this.tailArr.push({x:null,y:null})
     }
     
+    //지렁이 위치를 변경해주는 함수
     this.updateSnake = function() {
-                
+        //꼬리 위치 변경
         if(this.tailArr.length > 0){
             var i;
             for ( i = this.tailArr.length - 1; i > 0; i -= 1) {
@@ -41,51 +43,69 @@ var Snake = function() {
             }
         }
         this.tailArr[0] = {x: this.x, y: this.y}
-                
+         
+        //머리 위치 변경
         this.x += this.xv * scl;
         this.y += this.yv * scl; 
             
+        //맵 밖으로 나가지 않도록 보정
         if( this.x < 0 ) { this.x = 0 }
         if( this.x + 10 > vcanvas.width ) { this.x = vcanvas.width - 10 }
         if( this.y < 0 ) { this.y = 0 }
         if( this.y + 10 > vcanvas.height ) { this.y = vcanvas.height - 10 }
+    
+        
     }
 
+    //방향을 지정하는 함수
     this.dir = function( x,y ) {
         this.xv = x;
         this.yv = y;
     }
 
-
-    this.drawSnake = function() {
-        
-        if(this.tailArr.length > 1){
-            var i;
-            ctx.fillStyle = "gray";
-            for ( i = 0; i < this.tailArr.length-1; i++ ){
-                ctx.beginPath();
-                ctx.fillRect(this.tailArr[i].x, this.tailArr[i].y, scl, scl);
-            }
-        }
-
-        ctx.fillStyle = "black";
-        ctx.fillRect(this.x, this.y, scl, scl);
-    }
 }
 
+//지렁이를 그리는 함수 - 지렁이 배열의 요소 인덱스를 매개변수로 받아서 아이디가 같으면 검정색, 아니면 파란색으로 그려줌
+function drawSnake(snakeIndex) {
+    var i;
+    if(snakeIndex.id === Snake.id){
+        ctx.fillStyle = "gray";
+    }
+    else{
+        ctx.fillStyle = "skyblue";
+    }    
+    var i;
+    for ( i = 0; i < snakeIndex.tailArr.length-1; i++ ){
+        ctx.fillRect(snakeIndex.tailArr[i].x, snakeIndex.tailArr[i].y, scl, scl);
+    }
+
+    if(snakeIndex.id === Snake.id){
+        ctx.fillStyle = "black";
+    }
+    else{
+        ctx.fillStyle = "blue";
+    }
+    ctx.fillRect(snakeIndex.x, snakeIndex.y, scl, scl);
+}
+
+//게임의 프레임마다 실행시키는 함수
 function gameLoop() {
-    clearCanvas();
-    update();
-    Snake.updateSnake();
-
-    createFeed();
-    eatFeed( Snake );
-    drawFeed();
+    var i = 0;
+    sendData();                     //지렁이 데이터를 서버로 보냄
+    clearCanvas();                  //화면 지우기
     
-    Snake.drawSnake();
+    createFeed();                   //먹이 생성
+    drawFeed();                     //먹이 그리기
+    eatFeed( Snake );               //지렁이가 먹이 먹었는지 확인해서 먹으면 삭제
+    
 
-    // console.log(Snake.id)
-    sendData();
+    for(i; i<SnakeArr.length; i++)  //지렁이 배열에 있는 모든 지렁이 그리기(본인 아이디면 검정, 아니면 파랑으로 그림)
+    { 
+        drawSnake(SnakeArr[i]);
+    }
+
+    update();                       //키 입력을 받아 방향 설정
+    Snake.updateSnake();            //변경된 위치 적용
 }
 
 function init() {
@@ -93,64 +113,72 @@ function init() {
     vcanvas = document.getElementById('myCanvas');
     ctx = vcanvas.getContext('2d');
 
-    Snake = new Snake();
-    SnakeArr.push(Snake)
-
-
-    setInterval(gameLoop,100);
-    // setInterval(createFeed,100);
+    Snake = new Snake();            //새로운 지렁이 생성
+    SnakeArr.push(Snake)            //지렁이 배열에 추가
+    setInterval(gameLoop,100);      //게임 루프
 }
 
-
 //feed
+
+//먹이 생성하는 함수
 function createFeed() {
-    const fx = Math.floor(Math.random() * vcanvas.width - scl)
-    const fy = Math.floor(Math.random() * vcanvas.height - scl)
-    const fc = "#" + parseInt(Math.random() * 0xffffff).toString(16)
-    let findX, findY;
+    const fx = Math.floor(Math.random() * vcanvas.width - scl)       //먹이의 x값 랜덤 설정
+    const fy = Math.floor(Math.random() * vcanvas.height - scl)      //먹이의 y값 랜덤 설정
+    const fc = "#" + parseInt(Math.random() * 0xffffff).toString(16) //먹이의 색상 랜덤 설정
+    let findX, findY;   //먹이 배열에서 중복된 값 있는지 판단하기 위한 변수 생성
 
     for(let i = 0; i < feedArr.length; i++){
         findX = feedArr[i].x == fx;
         findY = feedArr[i].y == fy;
     }
     
-    if(findX && findY )
+    if(findX && findY ) //이미 먹이 배열에 x와 y값이 동일한 중복된 값이 있으면 값을 추가하지 않는다.
     {
         return;
     }
-    const newFood = { x:fx, y:fy, c:fc };
-    // feedArr.push(newFood);
-    socket.emit('feedLocation',newFood);
+    const newFood = { x:fx, y:fy, c:fc };   //먹이 배열에 없으면 새로운 객체 생성
+    feedArr.push(newFood);                  //먹이 배열에 생성한 객체 추가
+    socket.emit('feedLocation',feedArr);    //먹이 배열을 서버로 보낸다.
 }
 
+//먹이를 그려주는 함수
 function drawFeed() {
     var i;
-    for(i = 0; i < feedArr.length; i++){
-        if(feedArr[i].x % scl == 0 && feedArr[i].y % scl == 0){
-            ctx.beginPath();
+    for(i = 0; i < feedArr.length; i++)     //먹이 배열 길이 만큼 반복
+    {               
+        if(feedArr[i].x % scl == 0 && feedArr[i].y % scl == 0) //일정한 간격을 만족하는 먹이만 그린다.
+        {
             ctx.fillStyle = feedArr[i].c;
             ctx.fillRect(feedArr[i].x, feedArr[i].y, scl, scl);
         }
-    }
-}
-
-function eatFeed( snake ) {
-    var i;
-    for(i = 0; i < feedArr.length; i++){
-        if(feedArr[i].x == snake.x && feedArr[i].y == snake.y ){
-            snake.addTail();
-            feedArr.splice(i,1);
+        else{
+            feedArr.splice(i,1)   //아니면 삭제
         }
     }
-    // socket.emit('feedLocation',feedArr);
 }
 
-//Snake
+//먹이를 먹었을 때 처리하는 함수
+function eatFeed( snake ) {
+    var i;
+    for(i = 0; i < feedArr.length; i++){    
+        if(feedArr[i].x == snake.x && feedArr[i].y == snake.y ){    //지렁이 x,y 값이 먹이 x,y값과 일치하면
+            snake.addTail();                                        //꼬리 추가
+            feedArr.splice(i,1);                                    //먹이 삭제
+        }
+    }
+    socket.emit('feedLocation',feedArr);                            //먹이가 삭제된 배열을 서버로 보냄
+}
 
+
+
+//화면 지워주는 함수
 function clearCanvas() {
     ctx.clearRect(0,0,vcanvas.width,vcanvas.height);
-}
 
+}
+//Snake
+
+//뱀의 방향 설정
 function update() {
     if(key.left) { Snake.dir(-1,0)}
     if(key.right) { Snake.dir(1,0)}
@@ -178,60 +206,36 @@ document.onkeydown = set_key;
 
 //socket 
 
+//서버의 뱀 배열 자체를 받아와서 클라이언트의 뱀 배열에 저장
 socket.on('snakeLocation', (snakeArrData) => {
-    // console.log(snakeArrData[0])
-    // for(let i = 0; i < snakeArrData.length; i++){
-    //     if(snakeArrData.id == Snake.id){
-    //         Snake.x = snakeArrData.x;
-    //         Snake.y = snakeArrData.y;
-    //         Snake.tailArr = snakeArrData.tailArr;
+    SnakeArr = snakeArrData;
+    
+    // snakeArrData.forEach((e)=>{
+    //     if(e.id === Snake.id){
+    //         var index = snakeArrData.indexOf(e);
+    //         snakeArrData.splice(index,1);
+    //         for(var j in snakeArrData){
+    //             ctx.fillStyle = "blue"
+    //             ctx.fillRect(snakeArrData[j].x, snakeArrData[j].y, scl, scl);
+    //         }
     //     }
-    // }
-    snakeArrData.forEach((e)=>{
-        if(e.id === Snake.id){
-            var index = snakeArrData.indexOf(e);
-            snakeArrData.splice(index,1);
-            for(var j in snakeArrData){
-                ctx.fillStyle = "blue"
-                ctx.fillRect(snakeArrData[j].x, snakeArrData[j].y, scl, scl);
-                ctx.fillStyle = "black"
-            }
-        }
-        // Snake.x = e.x;
-        // Snake.y = e.y;
-        // Snake.tailArr = e.tailArr;
-        // console.log(e.id)
-    })
-    // console.log(SnakeArr)
+    // })
     
 });
 
-// socket.on('getId',(dataId)=>{
-//     Snake.id = dataId
-// })
-socket.on('feedLocation', (foodData) => {
-    //feedArr = foodData
-    const num = feedArr.findIndex((i) => {
-        return i.x == foodData.x && i.y == foodData.y;
-    });
-    if(num == -1)
-        feedArr.push(foodData);
+//서버의 먹이 배열 자체를 받아와서 클라이언트의 먹이 배열에 저장
+socket.on('feedLocation', (foodArrData) => {
+    feedArr = foodArrData
+    // const num = feedArr.findIndex((i) => {
+    //     return i.x == foodData.x && i.y == foodData.y;
+    // });
+    // if(num == -1)
+    //     feedArr.push(foodData);
 });
 
+//서버에서 키 데이터를 받아서 클라이언트의 키에 적용
 socket.on('keyEvent',(keyData)=>{
     key = keyData;
 });
 
-// socket.on('sendFeed',(feedArrData)=>{
-//     feedArr = feedArrData;
-// })
-
-// socket.on('snakeArr', (snakeData) => {
-//     const num = SnakeArr.findIndex((i) => {
-//         console.log(i.id)
-//         return i.id == snakeData.id
-//     });
-//     if(num == -1)
-//         SnakeArr.push(snakeData);
-// });
 
--- multi_slither_answer/server.js
+++ multi_slither_answer/server.js
@@ -13,65 +13,46 @@ var io = socket(server);
 
 io.on('connection', (socket) => {
     console.log('connected');
-    // console.log(socket.id);
-
-    // id = socket.id;
-    // console.log(id)
    
+    //id를 클라이언트로 보냄
     socket.emit('getId',socket.id);
-    // if(id.length == 0){
-    //     id.push(socket.id)
-    //     socket.emit('getId',id[0]);
-    // }
-    
-    // id.forEach(function(e){
-    //     if(e === socket.id){
-    //         var index = id.indexOf(e);
-    //         io.emit('getId',id[index])
-    //     }
-    // })
-        
-    
-    // socket.on('snakeArr',(dataSnake)=>{
-    //     io.emit('snakeArr', dataSnake)
-    // })
-
-    // socket.emit('sendFeed', (dataFood)=>{
-    //     io.emit('sendFeed', dataFood);
-    // })
-    
-
+   
     socket.on('snakeLocation', (dataSnake) => {
-        // console.log(id)
+        //처음 클라이언트가 접속했을 때 지렁이에 대한 정보 추가
         if(players.length === 0){
             players.push(dataSnake);
         }else{
+            //이후 클라이언트 접속 시 배열의 길이를 확인
             for(var j = 0; j < players.length; j++){
+                //만약 players[j]번째 해당하는 지렁이id와 요청받은 지렁이id가 일치하면 player[j]번째에 요청받은 지렁이에 대한 정보를 넣고 for문을 종료한다.
                 if(players[j].id === dataSnake.id){
                     players[j] = dataSnake;
                     break;
                 }
+                //만약 players 배열에 id가 일치하는 지렁이 정보가 없으면(배열의 마지막까지 반복했을 때 값이 없으면) 입력받은 지렁이 정보를 배열에 추가한다.
                 if(j===players.length - 1){
                     players.push(dataSnake);
                 }
             }
-            
         }
+        //새롭게 수정한 클라이언트들의 정보가 담긴 배열을 웹 소켓에 접속한 모든 플레이어들에게 보낸다.
         io.emit('snakeLocation', players);
     });
 
+    //키 요청을 받아서 각 클라이언트에게 따로 보낸다.
     socket.on('keyEvent',(dataKey)=>{
         socket.emit('keyEvent', dataKey);
     });
 
-    socket.on('feedLocation', (dataFood) => {
-        io.emit('feedLocation', dataFood);
+    //먹이 배열에 대한 정보를 받아 모든 클라이언트에게 보낸다.
+    socket.on('feedLocation', (dataFoodArr) => {
+        io.emit('feedLocation', dataFoodArr);
     });
 
-    
-
+    //클라이언트 접속 종료 시 
     socket.on('disconnect', () => {
         console.log('disconnected');
+        //클라이언트들의 정보를 담고 있는 players 배열의 요소 중 아이디가 일치하는 것을 삭제한다.
         players.forEach((element)=>{
             if(element.id === socket.id){
                 var index = players.indexOf(element);
